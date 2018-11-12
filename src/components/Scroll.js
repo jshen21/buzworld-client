d85498327d7e0765fc1b37db8e32c04c521ddfab
Jinyan <jinyan.shen21@gmail.com>
2018-11-11 19:32:47 -0500

    Remove comment out lines

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,6 @@ const Scroll = (props) => {
   return (
     <div style={{
           overflowY: 'scroll',
-        //   borderTop: '2px solid gray',
           backgroundColor: 'rgb(252, 252, 252)',
           margin: 'auto',
           width: '75%',
